312c0880edc100e3f21bd221ca2f9e103447a794
pengyongfu <1439762372@qq.com>
2023-08-30 21:47:27 +0800

    高并发

--- src/views/types/main.vue
+++ src/views/types/main.vue
@@ -223,6 +223,16 @@
         <myImg></myImg>
       </div>
     </el-card>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          高并发
+        </div>
+      </template>
+      <div>
+
+      </div>
+    </el-card>
 
 
   </div>
